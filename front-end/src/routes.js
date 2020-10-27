781d9dd9f33ecaa3a35adedb8309d82012268b1b
Marcos <mtomasferreira@hotmail.com>
2020-10-26 23:05:12 -0300

    beginning applier page

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import CadastroCandidato from './Pages/CadastroCandidato';
+import CadastroEmpresa from './Pages/CadastroEmpresa';
 
 function Routes(){
     return(
         <BrowserRouter>
             <Route path="/" exact component={Home} />
             <Route path="/login" component={Login} />
+            <Route path="/cadastro" component={CadastroCandidato} />
+            <Route path="/cadastroempresa" component={CadastroEmpresa} />
         </BrowserRouter>
     );
 }
