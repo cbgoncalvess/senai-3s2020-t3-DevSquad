7208b7cc35b16b5dd73957d8df6044e51fcba695
FIC <FIC@info.local>
2020-11-04 16:59:39 -0200

    Ajuste style Tela Login

--- front-end/src/Pages/Login/index.js
+++ front-end/src/Pages/Login/index.js
@@ -55,27 +55,27 @@ export default function Login() {
                                 <p className="sub-titulo">Bem-vindo ao SENAI | TechVagas</p>
                             </div>
 
-                            <div>
-                                <form className="form-logar" onSubmit={event => {
-                                    event.preventDefault();
-                                    login();
-                                }}>
 
-                                    <div className="divisionCampo">
-                                        <label>Usuário ou E-mail:</label>
-                                        <input type="text" name="email" className="inputUser" placeholder="exemplo@exemplo.com / mariasantos" onChange={e => setEmail(e.target.value)} />
-                                    </div>
+                            <form className="form-logar" onSubmit={event => {
+                                event.preventDefault();
+                                login();
+                            }}>
 
-                                    <div className="divisionCampo divisionPassword">
-                                        <label>Senha:</label>
-                                        <input type="password" name="password" placeholder="******" className="inputPassword" onChange={e => setSenha(e.target.value)} />
-                                        <Link className="recuperarPassword" to="/">Recuperar senha</Link>
-                                    </div>
-                                </form>
-                            </div>
+                                <div className="divisionCampo">
+                                    <label>Usuário ou E-mail:</label>
+                                    <input type="text" name="email" className="inputUser" placeholder="exemplo@exemplo.com / mariasantos" onChange={e => setEmail(e.target.value)} />
+                                </div>
+
+                                <div className="divisionCampo divisionPassword">
+                                    <label>Senha:</label>
+                                    <input type="password" name="password" placeholder="******" className="inputPassword" onChange={e => setSenha(e.target.value)} />
+                                    <Link className="recuperarPassword" to="/">Recuperar senha</Link>
+                                </div>
+                            </form>
+
 
                             <div className="divisionBtn">
-                                <button className="btnNew"><Link to="/cadastro">criar conta</Link></button>
+                                <button className="btnNew"><Link className="link-cad-conta" to="/cadastro">criar conta</Link></button>
                                 <button className="btnLogar" onClick={login}>entrar</button>
                             </div>
                         </div>
