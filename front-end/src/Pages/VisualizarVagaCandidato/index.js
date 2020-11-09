3d70a90168ff77130c789b019f54756a9c2fde13
FIC <FIC@info.local>
2020-11-08 23:35:01 -0200

    Tela VisualizarVagaCompletaCandiato estruturada ajax, funcional, com style

--- front-end/src/Pages/VisualizarVagaCandidato/index.js
+++ front-end/src/Pages/VisualizarVagaCandidato/index.js
@@ -19,8 +19,13 @@ import imgSalario from '../../assets/money (1).png';
 import imgTipoContrato from '../../assets/gears.png';
 import imgFuncao from '../../assets/rocket-launch.png';
 import IconEmpresa from '../../assets/building.png';
+import { useHistory } from 'react-router-dom';
 
 export default function VisualizarVaga() {
+
+    
+    let history = useHistory();
+    function meCandidatar() {history.push("/perfilCandidato");}
 
     const [idVaga, setIdVaga] = useState(0);
     const [Experiencia, setExperiencia] = useState('');
@@ -28,7 +33,7 @@ export default function VisualizarVaga() {
     const [Salario, setSalario] = useState('');
     const [DescricaoBeneficio, setDescricaoBeneficio] = useState('');
     const [DescricaoEmpresa, setDescricaoEmpresa] = useState('');
-    // const [DescricaoVaga, setDescricaoVaga] = useState('');
+    const [DescricaoVaga, setDescricaoVaga] = useState('');
     const [Tecnologias, setTecnologias] = useState([]);
     const [Estado, setEstado] = useState('');
     const [Cidade, setCidade] = useState('');
@@ -47,7 +52,6 @@ export default function VisualizarVaga() {
             setIdVaga(dados.idVaga);
             setLogradouro(dados.logradouro);
             setTipoContrato(dados.tipoContrato);
-            // SetDescricaoVaga(dados.descricaoVaga);
             setSalario(dados.salario);
             setTecnologias(dados.tecnologias);
             setDescricaoBeneficio(dados.descricaoBeneficio);
@@ -57,115 +61,91 @@ export default function VisualizarVaga() {
             setDescricaoEmpresa(dados.descricaoEmpresa);
             setCEP(dados.cep);
             setExperiencia(dados.experiencia);
+            setDescricaoVaga(dados.descricaoVaga);
         }).catch(err => console.error(err));
 
     }
 
-        return (
-            <div className="VisualizarVaga">
-                <AccessBar />
-                <Header />
-                <main className="sessaoVisualizarVaga">
+    return (
+        <div className="VisualizarVaga">
+            <AccessBar />
+            <Header />
+            <main className="sessaoVisualizarVaga">
 
-                    <section className="imgBannerDescriVaga">
+                <section className="imgBannerDescriVaga">
 
-                        <div className="divisionIntroVaga">
+                    <div className="divisionIntroVaga">
 
-                            <h2 className="v-titleVaga">Desenvolvedor Mobile Jr.</h2>
+                        <h2 className="v-titleVaga">{DescricaoVaga}</h2>
 
-                            <div className="divisionTagsLinguagem">
-                                {Tecnologias.map((item) => {
-                                    return (
-                                        <Tag NomeTag={item} />
-                                    );
-                                })
-                                }
+                        <div className="divisionTagsLinguagem">
+                            {Tecnologias.map((item) => {
+                                return (
+                                    <Tag NomeTag={item} />
+                                );
+                            })
+                            }
 
+                        </div>
+                    </div>
+
+                </section>
+
+                <section className="infoVagaVisualizar">
+                    <div className="icard-division">
+                        <img src={imgteste} alt="Logo da empresa" />
+
+                        <div className="divisionTagsVagas">
+
+                            <div className="card-vaga-info">
+                                <InfoVaga NomeProp={Logradouro} source={IconEmpresa} />
+                                <InfoVaga NomeProp={Complemento} source={imgLocalizacao} />
+                                <InfoVaga NomeProp={Experiencia} source={imgFuncao} />
+                            </div>
+
+                            <div className="card-vaga-info">
+                                <InfoVaga NomeProp={TipoContrato} source={imgTipoContrato} />
+                                <InfoVaga NomeProp={Salario} source={imgSalario} />
+                                <InfoVaga NomeProp={"Área de desenvolvimento"} source={imgDesenvolvimento} />
                             </div>
                         </div>
-
-                    </section>
-
-                    <section className="infoVagaVisualizar">
-                        <div className="icard-division">
-                            <img src={imgteste} alt="Logo da empresa" />
-
-                            <div className="divisionTagsVagas">
-
-                                <div className="card-vaga-info">
-                                    <InfoVaga NomeProp={"Teste"} source={IconEmpresa} />
-                                    <InfoVaga NomeProp={"Teste"} source={imgLocalizacao} />
-                                    <InfoVaga NomeProp={"Teste"} source={imgFuncao} />
-                                </div>
-
-                                <div className="card-vaga-info">
-                                    <InfoVaga NomeProp={"Teste"} source={imgTipoContrato} />
-                                    <InfoVaga NomeProp={"Teste"} source={imgSalario} />
-                                    <InfoVaga NomeProp={"Teste"} source={imgDesenvolvimento} />
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    </div>
+                </section>
 
 
-                    <section className="sessao-svempresa">
+                <section className="sessao-svempresa">
 
-                        <div className="descri-empresa">
-                            <h2>Descrição da empresa</h2>
-                            <p>Microsoft Corporation é uma empresa transnacional americana com sede em Redmond,
-                            Washington, que desenvolve, fabrica, licencia, apoia e vende softwares de computador,
-                        produtos eletrônicos, computadores e serviços pessoais</p>
-                        </div>
+                    <div className="descri-empresa">
+                        <h2>Descrição da empresa</h2>
 
-                        <div className="descri-vaga">
-                            <h2>Requisitos da vaga</h2>
-                            <p>Requisitos Técnicos:
-                            *Conhecimento intermediário em Javascript e framework Angular.
-                            *Conhecimentos em ES6.
-                            *Experiência com desenvolvimento mobile com Ionic / Cordova.
-                            *Conhecimento em controle de versão Git.
-                            *Inglês técnico. (Leitura e escrita).
-                            *Bom entendimento sobre APIs RESTful.
-                            *Domínio em HTML5 & CSS3 (SASS).
+                        <p>{DescricaoEmpresa}</p>
+                    </div>
 
-                            Requisitos Adicionais:
-                            *Conhecimento básico em PHP;
-                            *Experiência com Gulp;
-                            *Conhecimento em VueJS;
-                            *Participação ativa no GitHub.
-                            *+100pts no StackOverflow.
-                        *Gostar de rock n’ roll!</p>
+                    <div className="descri-vaga">
+                        <h2>Requisitos da vaga</h2>
+                        <p>{DescricaoVaga}</p>
 
-                        </div>
+                    </div>
 
-                    </section>
+                </section>
 
-                    <section className="divisionBeneVaga">
-                        <div className="centerBene">
-                            <h2>O que oferecemos</h2>
+                <section className="divisionBeneVaga">
+                    <div className="centerBene">
+                        <h2>O que oferecemos</h2>
+
+                        <div className="divisionPlan">
 
                             <div className="divisionPlan">
-                                <div>
-                                    <p>
-                                        -Plano Odontológico
-                                        -Plano de Saúde
-                                        -Vale Alimentação
-                                        -Vale transporte
-                                -Vale refeição</p>
-                                </div>
-
-                                <div className="divisionPlan">
-                                    <p>-Incentivo para palestrar e participar de eventos.
-                                   -Flexibilidade para trabalho remoto.</p>
-                                </div>
-
+                                <p>{DescricaoBeneficio}</p>
                             </div>
-                            <button className="btnCandidatase" type="submit">Me Candidatar</button>
-                        </div>
-                    </section>
 
-                </main>
-                <Footer />
-            </div>
-        )
-    }
+                        </div>
+                        <button className="btnCandidatase" type="submit" onClick={meCandidatar}>Me Candidatar</button>
+                    </div>
+                </section>
+
+            </main>
+            <Footer />
+        </div>
+    )
+}
