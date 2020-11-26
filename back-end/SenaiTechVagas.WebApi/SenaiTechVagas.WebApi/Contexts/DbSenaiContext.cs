00da07e0bf4bfbd0d9792f69df2b2129d2dd0c57
FIC <FIC@info.local>
2020-11-25 23:23:06 -0200

    Ajustar merge

--- back-end/SenaiTechVagas.WebApi/SenaiTechVagas.WebApi/Contexts/DbSenaiContext.cs
+++ back-end/SenaiTechVagas.WebApi/SenaiTechVagas.WebApi/Contexts/DbSenaiContext.cs
@@ -35,7 +35,7 @@ namespace SenaiTechVagas.WebApi.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-0VF65US\\SQLEXPRESS; Initial Catalog=Db_TechVagas;integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=DESK-02-10-14\\SQLEXPRESS2019; Initial Catalog=Db_TechVagas; user Id=sa; pwd=sa@132");
             }
         }
 
