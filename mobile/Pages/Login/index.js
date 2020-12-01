5d450ae58e26dc3b3b59afb2187ccb7e9c4d7754
FIC <FIC@info.local>
2020-11-30 23:52:52 -0200

    Estruturando page Login

---
+++ mobile/Pages/Login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {ImageBackground,StyleSheet,Text, View, Image,} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export default function Login({navigation}) {
+    return(
+        <View style={styles.login}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    login: {
+
+
+    }});
