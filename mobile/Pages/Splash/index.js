7de849fdb268f1c29fb0c79f93a14fe5e7c3a8c7
Marcos <mtomasferreira@hotmail.com>
2020-12-05 21:55:33 -0300

    data not to up

--- mobile/Pages/Splash/index.js
+++ mobile/Pages/Splash/index.js
@@ -1,18 +1,17 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Text, StatusBar } from "react-native";
 
 import icone from "../../assets/Images/Logo_TecVagas_Mobile.png";
 import styles from "./style";
 
 export default function Splash({ navigation }) {
-  
-  setTimeout(() => navigation.navigate("Login"), 2000);
+  setTimeout(() => navigation.navigate("Login"), 10000);
 
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#4f6d7a" barStyle="light-content" />
-      <Text style={styles.texto} >SENAI | TechVagas</Text>
-      {/* <Image source={icone}></Image> */}
+      <Text style={styles.texto}>SENAI | TechVagas</Text>
+      <Image source={icone}></Image>
     </View>
   );
 }
